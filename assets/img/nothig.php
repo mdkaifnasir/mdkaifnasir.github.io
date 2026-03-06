f46b395c28ab0acf939525288913b6bea49e67c5
Mdkaif250 <mdkaifnasir@gmail.com>
2026-03-06 14:45:50 +0530

    Deploy V2 Portfolio - Single File Glassmorphism

---
+++ assets/img/nothig.php
@@ -0,0 +1,30 @@
+/* Me */
+
+position: absolute;
+width: 258px;
+height: 259px;
+left: 461px;
+top: 305.96px;
+
+
+
+/* Ellipse 3 */
+
+position: absolute;
+width: 258px;
+height: 259px;
+left: 461px;
+top: 305.96px;
+
+background: radial-gradient(50% 50% at 50% 50%, #FFFFFF 17.71%, rgba(67, 67, 67, 0) 100%);
+
+
+/* image 1 */
+
+position: absolute;
+width: 165px;
+height: 223px;
+left: 509px;
+top: 311.68px;
+
+background: url(image.png);
